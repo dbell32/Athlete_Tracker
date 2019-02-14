af23b251239d85e1bf9d55de72cc3fe7f84c3f5f
Darren Bell <dbellvr@gmail.com>
2019-02-13 21:25:29 -0500

    Initial Commit

---
+++ Athlete_Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Athlete_Tracker
+//
+//  Created by Local Admin on 2/13/19.
+//  Copyright © 2019 Student Athlete Academy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
